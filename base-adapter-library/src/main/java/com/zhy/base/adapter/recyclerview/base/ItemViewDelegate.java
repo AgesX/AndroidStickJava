a01bb92186a3b1338e72971cb19841a2b372e3a3
邓江洲 <dengjiangzhouReal@gmail.com>
2021-05-09 11:01:35 +0800

    start Android X 迁移

---
+++ base-adapter-library/src/main/java/com/zhy/base/adapter/recyclerview/base/ItemViewDelegate.java
@@ -0,0 +1,19 @@
+// package com.zhy.adapter.recyclerview.base;
+package com.zhy.base.adapter.recyclerview.base;
+
+
+import android.view.View;
+
+/**
+ * Created by zhy on 16/6/22.
+ */
+public interface ItemViewDelegate<T>
+{
+
+    int getItemViewLayoutId();
+
+    boolean isForViewType(T item, int position);
+
+    void convert(ViewHolder holder, T t, int position);
+
+}
